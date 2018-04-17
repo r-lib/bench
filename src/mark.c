b43e8ae6b3a521ce0f823ee0a25c8fd51d9099aa
Jim Hester <james.f.hester@gmail.com>
2018-04-17 12:18:56 -0400

    Capture gcinfo per run

--- src/mark.c
+++ src/mark.c
@@ -1,11 +1,17 @@
 #include <Rinternals.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "nanotime.h"
+#include <unistd.h>
 
-SEXP mark_(SEXP expr, SEXP env, SEXP min_time, SEXP num_iterations) {
+SEXP mark_(SEXP expr, SEXP env, SEXP min_time, SEXP num_iterations, SEXP logfile) {
   R_xlen_t n = INTEGER(num_iterations)[0];
   double min = REAL(min_time)[0];
 
-  SEXP times = PROTECT(Rf_allocVector(REALSXP, n));
+  const char* log = CHAR(STRING_ELT(logfile, 0));
+  SEXP out = PROTECT(Rf_allocVector(VECSXP, 2));
+  SET_VECTOR_ELT(out, 0, Rf_allocVector(REALSXP, n));
+  SET_VECTOR_ELT(out, 1, Rf_allocVector(STRSXP, n));
 
   long double begin;
   if (NANO_UNEXPECTED(nano_time(&begin))) {
@@ -18,7 +24,21 @@ SEXP mark_(SEXP expr, SEXP env, SEXP min_time, SEXP num_iterations) {
       Rf_error("Failed to get start time iteration: %i", i);
     }
 
+    freopen(log, "w", stderr);
+
+    // Actually evaluate the R code
     Rf_eval(expr, env);
+
+    FILE* fp = fopen(log, "r");
+    char* buffer = NULL;
+    size_t len;
+    ssize_t bytes_read = getdelim( &buffer, &len, '\0', fp);
+    if ( bytes_read != -1) {
+      SET_STRING_ELT(VECTOR_ELT(out, 1), i, Rf_mkChar(buffer));
+      free(buffer);
+    }
+    fclose(fp);
+
     if (NANO_UNEXPECTED(nano_time(&end))) {
       Rf_error("Failed to get end time iteration: %i", i);
     }
@@ -26,18 +46,21 @@ SEXP mark_(SEXP expr, SEXP env, SEXP min_time, SEXP num_iterations) {
     // If we are over our time threshold for this expression break
     if (min != 0 && end - begin > min) { break; }
 
-    REAL(times)[i] = end - start;
+    REAL(VECTOR_ELT(out, 0))[i] = end - start;
   }
 
-  times = Rf_lengthgets(times, i);
+  SET_VECTOR_ELT(out, 0, Rf_lengthgets(VECTOR_ELT(out, 0), i));
+  SET_VECTOR_ELT(out, 1, Rf_lengthgets(VECTOR_ELT(out, 1), i));
+
+  freopen("/dev/tty", "a", stderr);
 
   UNPROTECT(1);
 
-  return times;
+  return out;
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"mark_", (DL_FUNC) &mark_, 4},
+    {"mark_", (DL_FUNC) &mark_, 5},
     {NULL, NULL, 0}
 };
 
