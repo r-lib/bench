69eb1ec57eb10ca0e8058a8232dd9be0afa4e289
Jim Hester <james.f.hester@gmail.com>
2018-04-19 09:31:44 -0400

    Add knit_print method to exclude data columns from printing by default
    This makes the results a little cleaner for things like stackoverflow if
    you are using reprex (or native knitr) to run your code

--- R/mark.R
+++ R/mark.R
@@ -216,3 +216,28 @@ auto_name <- function(exprs) {
 dots <- function(...) {
   eval(substitute(alist(...)))
 }
+
+#' Custom printing function for bench_mark objects in knitr documents
+#'
+#' By default data columns ('result', 'memory', 'time', 'gc') are omitted when
+#' printing in knitr. If you would like to include these columns set the knitr
+#' chunk option 'bench.all_columns = TRUE'.
+#' @param options A list of knitr chunk options set in the currently evaluated chunk.
+#' @inheritParams knitr::knit_print
+#' @details
+#' You can set `bench.all_columns = TRUE` to show all columns of the bench mark
+#' object.
+#'
+#'     ```{r bench.all_columns = TRUE}
+#'     bench::mark(
+#'       subset(mtcars, cyl == 3),
+#'       mtcars[mtcars$cyl == 3, ])
+#'     ```
+#' @export
+knit_print.bench_mark <- function(x, ..., options) {
+  if (isTRUE(options$bench.all_columns)) {
+    print(x)
+  } else {
+    print(x[!colnames(x) %in% data_cols])
+  }
+}
