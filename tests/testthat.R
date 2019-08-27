a87eb14ddad459132ca5e363a0b67951cfcbaaa0
Jim Hester <james.f.hester@gmail.com>
2019-08-27 15:27:11 -0400

    Revert "Setup azure pipelines"
    I don't actually want to use azure on this for now
    
    This reverts commit 9f631214b236be0bc09087088da3e6873fed90f5.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(bench)
 
-if (requireNamespace("xml2")) {
-  test_check("bench", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
-} else {
-  test_check("bench")
-}
+test_check("bench")
