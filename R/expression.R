d6732396e86b172c48aee7854cf0e65efb4a2b71
DavisVaughan <davis@rstudio.com>
2023-05-02 15:13:48 -0400

    Forward to `pillar::pillar_shaft.character()` for `bench_expr`s
    This ensures we get truncation that matches how it works for character vectors, which is generally pretty aggressive since you can often figure out which expression you are looking at from the first few characters, and it is more important to try and show more columns rather than showing everything contained in very long expressions.

--- R/expression.R
+++ R/expression.R
@@ -47,7 +47,10 @@ vec_restore.bench_expr <- function(x, to, ...) {
 }
 
 pillar_shaft.bench_expr <- function(x, ...) {
-  pillar::new_pillar_shaft_simple(format.bench_expr(x), align = "left", ...)
+  # We format bench expressions exactly like character vectors. This ensures
+  # they are truncated as needed, which is useful for long unnamed expressions
+  # (#94). This is the same logic as `pillar:::pillar_shaft.factor()`.
+  pillar::pillar_shaft(as.character(x), ...)
 }
 
 scale_type.bench_expr <- function(x) {
