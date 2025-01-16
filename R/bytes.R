db1c87c8ff78e0e3267e030354438e354b80dae4
Davis Vaughan <davis@posit.co>
2025-01-16 11:21:23 -0500

    Tweak formatting

--- R/bytes.R
+++ R/bytes.R
@@ -169,8 +169,11 @@ type_sum.bench_bytes <- function(x) {
 bench_bytes_trans <- function(base = 2) {
   if (is.null(base)) {
     return(
-      scales::trans_new("bch:byt", transform = as.numeric,
-        inverse = as_bench_bytes, breaks = scales::pretty_breaks(),
+      scales::trans_new(
+        name = "bch:byt",
+        transform = as.numeric,
+        inverse = as_bench_bytes,
+        breaks = scales::pretty_breaks(),
         domain = c(1e-100, Inf)
       )
     )
@@ -178,9 +181,13 @@ bench_bytes_trans <- function(base = 2) {
   trans <- function(x) log(as.numeric(x), base)
   inv <- function(x) as_bench_bytes(base ^ as.numeric(x))
 
-  scales::trans_new(paste0("bch:byt-", format(base)), transform = trans,
-    inverse = inv, breaks = scales::log_breaks(base = base),
-    domain = c(1e-100, Inf))
+  scales::trans_new(
+    name = paste0("bch:byt-", format(base)),
+    transform = trans,
+    inverse = inv,
+    breaks = scales::log_breaks(base = base),
+    domain = c(1e-100, Inf)
+  )
 }
 
 # Lazily registered in `.onLoad()`
