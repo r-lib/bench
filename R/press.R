2fb0d6b4419add4b10104c95e493f2fb2398987a
Davis Vaughan <davis@posit.co>
2025-01-16 16:02:18 -0500

    Use more explicit `tibble::tibble()` crossref
    To appease some CRAN checks

--- R/press.R
+++ R/press.R
@@ -15,9 +15,9 @@
 #'
 #' @param ... If named, parameters to define, if unnamed the expression to run.
 #'   Only one unnamed expression is permitted.
-#' @param .grid A pre-built grid of values to use, typically a [data.frame] or
-#'   [tibble]. This is useful if you only want to benchmark a subset of all
-#'   possible combinations.
+#' @param .grid A pre-built grid of values to use, typically a [data.frame()] or
+#'   [tibble::tibble()]. This is useful if you only want to benchmark a subset
+#'   of all possible combinations.
 #' @param .quiet If `TRUE`, progress messages will not be emitted.
 #' @export
 #' @examples
